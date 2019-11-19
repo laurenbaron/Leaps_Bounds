f35541abca5fec1400ea48fd30fcdab37655573c
laurenbaron <lbaron@udel.edu>
2019-11-19 08:52:13 -0500

    lily pads

--- Leaps_Bounds.py
+++ Leaps_Bounds.py
@@ -8,6 +8,24 @@ GAME_TITLE = "Froggy Road"
 GAME_SPEED = 1 / 60
 
 
+class LilySprite(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.texture=arcade.load_texture("images/lily.png", scale=.25)
+
+
+class LogSprite(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.texture=arcade.load_texture("images/log.jpg", scale=.25)
+
+
+class BoatSprite(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.texture=arcade.load_texture("images/boat.jpg", scale=.25)
+
+
 class Intro(arcade.Window):
     title: arcade.Sprite
     frog: arcade.Sprite
@@ -64,17 +82,21 @@ class Intro(arcade.Window):
 class MyGame(arcade.Window):
     speed: int
     frog: arcade.Sprite
-    lily: arcade.Sprite
-    log: arcade.Sprite
-    boat: arcade.Sprite
-    level1_width=WINDOW_WIDTH / 3
-    level2_width = WINDOW_WIDTH / 7
-    level3_width = WINDOW_WIDTH / 7
+    lily_list:arcade.SpriteList[LilySprite]
+    level1_width = WINDOW_WIDTH / 3
+    level23_width = WINDOW_WIDTH / 7
+    offset1=level1_width /2
+    offset23=level23_width /2
+    level_height=WINDOW_HEIGHT/6
+    y_offset=level_height/2
+    LILLIES=6
+    ROWS=3
 
     def __init__(self):
         """ Initialize variables """
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, GAME_TITLE)
         self.background=None
+        self.lily_list=[]
 
     def setup(self, user_speed):
         """ Setup the game (or reset the game) """
@@ -82,18 +104,34 @@ class MyGame(arcade.Window):
         arcade.set_background_color(BACKGROUND_COLOR)
         self.background=arcade.load_texture("images/water.jpg")
         self.frog = arcade.Sprite("images/frog.jpg", .25)
-        self.lily = arcade.Sprite("images/lily.png", .25)
-        self.log = arcade.Sprite("images/log.jpg", .25)
-        self.boat = arcade.Sprite("images/boat.jpg", .25)
+        self.lily_list = arcade.SpriteList()
+
+        #make lily pad grid
+        row=1
+        while row <= self.ROWS:
+            #dont draw lilies where boat going
+            if row==2:
+                pass
+            else:
+                index = 1
+                while index <= self.LILLIES:
+                    lily=LilySprite()
+                    lily.center_y=(self.level_height*index)-self.y_offset
+                    lily.center_x=(self.level1_width*row) - self.offset1
+                    self.lily_list.append(lily)
+                    index+=1
+            row+=1
+
 
     def on_draw(self):
         """ Called when it is time to draw the world """
         arcade.start_render()
         arcade.draw_texture_rectangle(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT, self.background)
-        self.frog.center_x = self.level1_width
-        self.frog.center_y = WINDOW_HEIGHT / 2
+        self.frog.center_x = self.level1_width - self.offset1
+        self.frog.center_y = self.level_height * 3 - self.y_offset
         self.frog.draw()
-
+        self.lily_list
+        self.lily_list.draw()
 
     def on_update(self, delta_time):
         """ Called every frame of the game (1/GAME_SPEED times per second)"""
